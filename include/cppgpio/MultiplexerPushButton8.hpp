f5a949211fd76a12fed06568871b1db9a1b6d349
BrunoCooper17 <BrunoCooper17@outlook.com>
2023-05-05 22:31:43 -0600

    Added MultiplexerPushButton8

---
+++ include/cppgpio/MultiplexerPushButton8.hpp
@@ -0,0 +1,115 @@
+//
+// Created by brunocooper on 5/5/23.
+//
+
+#ifndef CPPGPIO_MULTIPLEXERPUSHBUTTON8_H
+#define CPPGPIO_MULTIPLEXERPUSHBUTTON8_H
+
+
+#include <cppgpio.hpp>
+
+
+namespace GPIO
+{
+
+	struct FMultiplexerBool8
+	{
+		uint8_t bPinC : 1;
+		uint8_t bPinB : 1;
+		uint8_t bPinA : 1;
+	};
+
+
+	union UnionMultiplexer8
+	{
+		uint8_t Index;
+		FMultiplexerBool8 Bool;
+
+		explicit UnionMultiplexer8(uint8_t IndexValue)
+			: Index{ IndexValue }
+		{
+		}
+	};
+
+
+	/**
+	 * #todo: Add description
+	 * #todo: Move to a CPPGPIO fork and fix issues found (check todos below)
+	 * #todo: Refactor multiplexer out values so we can reuse it (and send an event to notify about new value to read)
+	 */
+	class MultiplexerPushButton8
+	{
+	public:
+		MultiplexerPushButton8(
+			/** Multiplexer Output pin */
+			uint32_t InPinInput,
+			/** Multiplexer pin A */
+			uint32_t InPinA,
+			/** Multiplexer pin B */
+			uint32_t InPinB,
+			/** Multiplexer pin C */
+			uint32_t InPinC,
+			/** Multiplexer check interval */
+			std::chrono::nanoseconds InSleepMultiplexerAction = std::chrono::microseconds(125),
+			/** Define interval between two events */
+			std::chrono::nanoseconds InMinTriggerAction = std::chrono::milliseconds(2),
+			/** Define minimum amount of time the hast to be triggered to consider it Hold action */
+			std::chrono::nanoseconds InMinHoldAction = std::chrono::milliseconds(750));
+
+		virtual ~MultiplexerPushButton8();
+
+		/**
+		 * Gets called when a button is pushed.
+		 * Register with a lambda expression or std::bind()
+		 */
+		std::function<void(uint8_t, const std::chrono::nanoseconds)> f_pushed{ nullptr };
+
+		/**
+		 * Gets called when a button is held.
+		 * Register with a lambda expression or std::bind()
+		 */
+		std::function<void(uint8_t, const std::chrono::nanoseconds)> f_held{ nullptr };
+
+		/**
+		 * From InputDetect::start (buttons.hpp)
+		 * After completion of class initialization (that is, registration of callables
+		 * at the various function variables), signal here that the input detection
+		 * thread could finally be run. If start() is never called, no detection will
+		 * ever happen.
+		 */
+		bool start();
+
+		/**
+		 * From InputDetect::stop (buttons.hpp)
+		 * it is also possible to stop the running thread, but you would normally not
+		 * need to do it - the destructor does it automatically. If you call stop(), then
+		 * be prepared that it could take up to 500ms for it to return (or even longer if
+		 * the thread is currently not waiting for an event, but executing some long
+		 * running custom task in your code).
+		 */
+		bool stop();
+
+
+	private:
+		GPIO::DigitalIn ButtonIn;
+		GPIO::DigitalOut MultiplexerPinA;
+		GPIO::DigitalOut MultiplexerPinB;
+		GPIO::DigitalOut MultiplexerPinC;
+
+		std::chrono::nanoseconds SleepMultiplexerAction{ std::chrono::microseconds(125) };
+		std::chrono::nanoseconds MinTriggerAction{ std::chrono::milliseconds(3) };
+		std::chrono::nanoseconds MinHeldAction{ std::chrono::milliseconds(750) };
+
+		std::array<std::chrono::steady_clock::time_point, 8> ButtonLastPushed;
+		std::array<bool, 8> ButtonLastState{ false };
+		std::array<bool, 8> ButtonCheckHeldState{ false };
+
+		bool bTerminate{ false };
+		std::unique_ptr<std::thread> thread_loop{ nullptr };
+
+		void event_loop();
+	};
+
+}; // namespace GPIO
+
+#endif // CPPGPIO_MULTIPLEXERPUSHBUTTON8_H
---
+++ src/MultiplexerPushButton8.cpp
@@ -0,0 +1,136 @@
+//
+// Created by brunocooper on 5/5/23.
+//
+
+#include "MultiplexerPushButton8.h"
+
+
+namespace GPIO
+{
+
+	MultiplexerPushButton8::MultiplexerPushButton8(uint32_t InPinOutput, uint32_t InPinA, uint32_t InPinB,
+												   uint32_t InPinC, std::chrono::nanoseconds InSleepMultiplexerAction, std::chrono::nanoseconds InMinTriggerAction,
+												   std::chrono::nanoseconds InMinHoldAction)
+		: ButtonIn{ InPinOutput, GPIO::GPIO_PULL::UP }
+		, MultiplexerPinA{ InPinA }
+		, MultiplexerPinB{ InPinB }
+		, MultiplexerPinC{ InPinC }
+		, SleepMultiplexerAction{ InSleepMultiplexerAction }
+		, MinTriggerAction{ InMinTriggerAction }
+		, MinHeldAction{ InMinHoldAction }
+	{
+	}
+
+
+	MultiplexerPushButton8::~MultiplexerPushButton8()
+	{
+		stop();
+	}
+
+
+	bool MultiplexerPushButton8::start()
+	{
+		if (thread_loop != nullptr)
+		{
+			return false;
+		}
+
+		thread_loop = std::make_unique<std::thread>(&MultiplexerPushButton8::event_loop, this);
+
+		return true;
+	}
+
+
+	bool MultiplexerPushButton8::stop()
+	{
+		if (thread_loop == nullptr)
+		{
+			return false;
+		}
+
+		bTerminate = true;
+
+		thread_loop->join();
+		thread_loop.reset(nullptr);
+
+		return true;
+	}
+
+
+	void MultiplexerPushButton8::event_loop()
+	{
+		UnionMultiplexer8 MultiplexerConvert{ 0 };
+
+		while (!bTerminate)
+		{
+			for (std::size_t idx{ 0 }; idx < ButtonLastPushed.size() && !bTerminate; ++idx)
+			{
+				MultiplexerConvert.Index = idx;
+				MultiplexerConvert.Bool.bPinA ? MultiplexerPinA.on() : MultiplexerPinA.off();
+				MultiplexerConvert.Bool.bPinB ? MultiplexerPinB.on() : MultiplexerPinB.off();
+				MultiplexerConvert.Bool.bPinC ? MultiplexerPinC.on() : MultiplexerPinC.off();
+
+				const bool InputRead{ ButtonIn.get_gpio_read() };
+				const auto TimeNow{ std::chrono::steady_clock::now() };
+
+				/** Reset the time in every unpressed button */
+				for (std::size_t internal_idx{ 0 }; internal_idx < ButtonLastPushed.size(); ++internal_idx)
+				{
+					if (internal_idx == idx)
+					{
+						continue;
+					}
+
+					if (!ButtonLastState[internal_idx])
+					{
+						ButtonLastPushed[internal_idx] = TimeNow;
+					}
+				}
+
+				/** Button was released, check the press valid (was it held? or at least was hold long enough for a tap)
+				 */
+				if (!InputRead)
+				{
+					const auto DeltaTime{ TimeNow - ButtonLastPushed[idx] };
+					if (ButtonCheckHeldState[idx] && DeltaTime >= MinTriggerAction)
+					{
+						ButtonLastPushed[idx] = TimeNow;
+
+						if (f_pushed)
+						{
+							f_pushed(idx, DeltaTime);
+						}
+					}
+				}
+
+				/** Update state if it's different than last state */
+				if (ButtonLastState[idx] != InputRead)
+				{
+					ButtonLastPushed[idx] = TimeNow;
+					ButtonLastState[idx] = InputRead;
+					ButtonCheckHeldState[idx] = InputRead;
+				}
+
+				/** Was it held long enough to consider it a hold action? */
+				if (ButtonCheckHeldState[idx])
+				{
+					const auto TimeHeld{ TimeNow - ButtonLastPushed[idx] };
+					if (TimeHeld >= MinHeldAction)
+					{
+						ButtonCheckHeldState[idx] = false;
+						ButtonLastPushed[idx] = TimeNow;
+
+						if (f_held)
+						{
+							f_held(idx, TimeHeld);
+						}
+					}
+				}
+
+				/** Wait until checking next value (to avoid cpu saturation) */
+				std::this_thread::sleep_for(SleepMultiplexerAction);
+			}
+		}
+	}
+
+} // namespace GPIO
